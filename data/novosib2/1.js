daf9831fb2f9e4786c4c564442b0bc14df2f2046
Jarrius <maksimrk@mail.ru>
2024-08-05 13:42:46 +0300

    initial commit nsk

---
+++ data/novosib2/1.js
@@ -0,0 +1,105 @@
+var lines = lines || {};
+lines['novosib2'] = lines['novosib2'] || {};
+lines['novosib2']['1'] = [
+  {
+    name: 'Золотая Нива',
+    trackLength: 1740,
+    arsDrawBreakpoint: 7,
+    arsAllSteps: true,
+    tStay: 25,
+    K: 1,
+    curves: {
+      0: 0,
+    },
+    slopes: {
+      0: 0,
+    },
+    modes: {
+    },
+    joints: [
+    ],
+    signals: [
+    ],
+  },
+  {
+    name: 'Березовая роща',
+    trackLength: 1120,
+    arsDrawBreakpoint: 9,
+    arsAllSteps: true,
+    tStay: 25,
+    K: 1,
+    curves: {
+      0: 0,
+    },
+    slopes: {
+      0: 0,
+    },
+    modes: {
+    },
+    joints: [
+    ],
+    signals: [
+    ],
+  },
+  {
+    name: 'Маршала Покрышкина',
+    trackLength: 1156,
+    arsAllSteps: true,
+    arsDrawBreakpoint: 8,
+    tStay: 25,
+    K: 1,
+    curves: {
+      0: 0
+    },
+    slopes: {
+      0: 0
+    },
+    modes: {
+    },
+    joints: [
+    ],
+    signals: [
+    ],
+  },
+  {
+    name: 'Сибирская',
+    trackLength: 1617,
+    arsAllSteps: true,
+    arsDrawBreakpoint: 12,
+    tStay: 25,
+    K: 1,
+    curves: {
+      0: 0,
+    },
+    slopes: {
+      0: 0,
+
+    },
+    modes: {
+    },
+    joints: [
+    ],
+    signals: [
+    ],
+  },
+  {
+    name: 'Пл. Гарина-Михайловского',
+    trackLength: 81,
+    arsAllSteps: true,
+    arsDrawBreakpoint: 8,
+    tStay: 25,
+    K: 1,
+    curves: {
+      0: 0,
+    },
+    slopes: {
+      0: 0,
+    },
+    modes: {
+    },
+    joints: [
+    ],
+    signals: [
+    ],
+  },
+]
