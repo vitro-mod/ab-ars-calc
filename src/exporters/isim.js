5272c6ac99d064419057a3d9c6d3c6bc594a8560
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2025-07-20 14:37:18 +0200

    isim export

---
+++ src/exporters/isim.js
@@ -0,0 +1,37 @@
+function signalRcs(signalName) {
+    const signal = peregon.signals.find(el => el.name == signalName);
+    const joint = peregon.joints.findIndex(el => el.name == signal.joint);
+    const open = peregon.joints.findIndex(el => el.name == (signal.y ?? signal.yg ?? signal.g));
+    const result = peregon.joints.slice(joint, open + 1).map(el => 'rc' + el.name);
+
+    return result;
+}
+
+function iSignals() {
+    let result = '';
+
+    for (let i = 0; i < peregon.signals.length; i++) {
+        const signal = peregon.signals[i];
+        let nextSignal = peregon.signals[i + 1];
+        if (nextSignal?.back) return result;
+
+        let aspectsNum = 2;
+        if (signal.y) aspectsNum = 3;
+        if (signal.yg) aspectsNum = 4;
+
+        if (signal.yg && !signal.y && !signal.g) {
+            nextSignal = null;
+        }
+
+        const nextString = nextSignal ? `next: 'sig${nextSignal.name.replaceAll('-', '')}', ` : '';
+        const rc = signalRcs(signal.name);
+        const rcString = "['" + rc.join("', '") + "']";
+        const resultString = `{ scheme: 'YYGR', name: 'sig${signal.name.replaceAll('-', '')}', params: {${nextString} rc: ${rcString}, aspectsNum: ${aspectsNum}} },\n`;
+
+        result += resultString;
+    }
+
+    console.log(result);
+
+    return result;
+}
--- src/signalEvents.js
+++ src/signalEvents.js
@@ -7,14 +7,12 @@ function setupSignalEvents() {
                 target.style.opacity = '1';
             }, 200);
 
-            let signal = peregon.signals.find(el => el.name == this.id);
-            let joint = peregon.joints.findIndex(el => el.name == signal.joint);
-            let open = peregon.joints.findIndex(el => el.name == (signal.y ?? signal.yg ?? signal.g));
-            let result = peregon.joints.slice(joint, open + 1).map(el => 'rc' + el.name);
-            let resultText = "['" + result.join("', '") + "']";
+            const signalName = this.id;
+            const result = signalRcs(signalName);
+            const resultText = "['" + result.join("', '") + "']";
             console.log(resultText);
             navigator.clipboard.writeText(resultText);
-        })
+        });
     });
 
     document.querySelectorAll('.rcname').forEach(el => {
@@ -33,7 +31,11 @@ function setupSignalEvents() {
 
                 const result = [];
                 for (let i = startI; i < endI; i++) {
-                    result.push(arsCode(peregon.joints[i]));
+                    if (e.shiftKey) {
+                        result.push('rc' + peregon.joints[i-1].name);
+                    } else {
+                        result.push(arsCode(peregon.joints[i]));
+                    }
                 }
 
                 const resultText = "'" + result.join("', '") + "'";
